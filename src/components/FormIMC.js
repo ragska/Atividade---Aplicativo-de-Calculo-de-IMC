af646293bb04a3982ce2221b6c190e3e098ee79d
Rafaela Kage <rafvalentina@gmail.com>
2025-04-03 22:55:28 -0300

    não ta funcionando o title (?)

---
+++ src/components/FormIMC.js
@@ -0,0 +1,68 @@
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import Result from './Result';
+import { useState } from 'react';
+
+const FormIMC = () => {
+
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+    const [imc, setIMC] = useState(null);
+
+    const calcularIMC = () => {
+        if (peso && altura) {
+            const alturaMetros = parseFloat(altura) / 100;
+            const imcCalculado = (parseFloat(peso) / (alturaMetros * alturaMetros)).toFixed(2);
+            setIMC(imcCalculado);
+        }
+    };
+
+    return (
+
+        <View style={styles.formContainer}>
+
+            <TextInput
+                style={styles.input}
+                placeholder='Peso (kg)'
+                keyboardType='numeric'
+                value={peso}
+                onChangeText={setPeso}
+            />
+
+            <TextInput
+                style={styles.input}
+                placeholder='Altura (cm)'
+                keyboardType='numeric'
+                value={altura}
+                onChangeText={setAltura}
+            />
+
+            <Button title="Calcular IMC" onPress={calcularIMC}/>
+
+            {imc && <Result imc={imc}/>}
+
+
+        </View>
+
+    );
+
+};
+
+const styles = StyleSheet.create({
+
+    formContainer: {
+        backgroundColor: '#f0f0f0',
+        padding: 16,
+        borderRadius: 10,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 12,
+        paddingHorizontal: 8,
+        borderRadius: 5,
+    },
+
+});
+
+export default FormIMC;
---
+++ src/components/Title.js
@@ -0,0 +1,20 @@
+import { Text, StyleSheet} from 'react-native';
+
+const Title = () => {
+
+    return(
+        <Text style ={styles.title}>Calculadora de IMC</Text>
+    );
+
+};
+
+const styles = StyleSheet.create({
+    title: {
+        frontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 24,
+    },
+});
+
+export default Title;
